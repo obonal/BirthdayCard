f355b6a82dbc0134252f454dd535f17740a91aa9
Olivier Bonal <olivier.bonal@gmail.com>
2021-02-02 00:24:30 +0000

    Better Theme

--- app/src/main/java/org/bonal/birthdaycard/ui/theme/Color.kt
+++ app/src/main/java/org/bonal/birthdaycard/ui/theme/Color.kt
@@ -2,7 +2,9 @@ package org.bonal.birthdaycard.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val purple200 = Color(0xFFBB86FC)
-val purple500 = Color(0xFF6200EE)
-val purple700 = Color(0xFF3700B3)
-val teal200 = Color(0xFF03DAC5)
+val Primary = Color(0xffbf360c)
+val Secondary = Color(0xffff7043)
+val PrimaryVariant = Color(0xff870000)
+val SecondaryVariant = Color(0xffc63f17)
+val OnPrimary = Color.White
+val OnSecondary = Color.Black
